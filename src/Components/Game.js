cfa3432b52a5fb8d21b63881bbf358c995575923
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-10 15:04:36 +0600

    Intractive Componenet

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -4,17 +4,17 @@ import Board from './Board';
 class Game extends Component {
     render() {
         return (
-          <div className="game">
-            <div className="game-board">
-              <Board />
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
             </div>
-            <div className="game-info">
-              <div>{/* status */}</div>
-              <ol>{/* TODO */}</ol>
-            </div>
-          </div>
         );
-      }
+    }
 }
 
 export default Game;
